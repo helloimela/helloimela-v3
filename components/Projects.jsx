8f3fe487833546fd4332845a32e30ce6cf881d43
helloimela <elayudhanira@gmail.com>
2023-01-16 18:53:47 +0100

    Fix: fix styling structure

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Story.scss';
+import './Projects.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { projects } from '../data/projects'
 
 const Projects = () => {
   return (
@@ -13,15 +14,18 @@ const Projects = () => {
         Something I do in my free time
       </h3>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <div className="swiper-content">
-            <h3 className="title">19 years ago</h3>
-            <h2 className="title">How it was started</h2>
-            <p className="first-paragraph ">
-            My first encounter with web development was when I was 13 years old. I learned to build a website using Microsoft Frontpage &amp; Dreamweaver, building HTML layout using tables. I even won several web design competitions during my junior high school time. Since then, I loved web design and everything related to web development. Now that I think about it, it’s amazing to see how web technology always evolving. From table layout now we can easily create layout with CSS grid.
-            </p>
-          </div>
-        </SwiperSlide>
+        {projects.map((project) => 
+          <SwiperSlide>
+            <div className="project-content">
+              <div className="big-img" style={{backgroundImage: 'url(http://placeimg.com/640/480/arch)'}}></div>
+              <div className="project-text">
+                <h2 className="title">{project.title}</h2>
+                <p className="first-paragraph ">{project.content}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        )}
+        
       </Swiper>
     </div>
   );
---
+++ data/projects.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: 'Droplets',
+    content:
+      'Droplet is the result of forming an IoT object through some critical design workshops involving plastic bottle. We have prototyped Droplet with a simple water filter using activated carbon as a filtering agent. This filter design can most definitely be improved upon, and also varied according to the particular needs of the user. The combination of water filter and water quality meter application enables Droplet as a supporting device for water quality data crowdsourcing. An assembly of sensors technology and mobile app, Droplet creates an opportunity for the community to engage in collecting water quality data at the same time get the benefit of accessing clean water through a portable and easy to use device. A future research about the use of Droplet as a crowdsourcing device should be conducted.',
+    link: 'http://droplet.helloimela.com/',
+    video: 'https://www.youtube.com/watch?v=QUkY421E9tM&t=61s',
+  },
+  {
+    title: 'Droplets',
+    content:
+      'Droplet is the result of forming an IoT object through some critical design workshops involving plastic bottle. We have prototyped Droplet with a simple water filter using activated carbon as a filtering agent. This filter design can most definitely be improved upon, and also varied according to the particular needs of the user. The combination of water filter and water quality meter application enables Droplet as a supporting device for water quality data crowdsourcing. An assembly of sensors technology and mobile app, Droplet creates an opportunity for the community to engage in collecting water quality data at the same time get the benefit of accessing clean water through a portable and easy to use device. A future research about the use of Droplet as a crowdsourcing device should be conducted.',
+    link: 'http://droplet.helloimela.com/',
+    video: 'https://www.youtube.com/watch?v=QUkY421E9tM&t=61s',
+  },
+];
